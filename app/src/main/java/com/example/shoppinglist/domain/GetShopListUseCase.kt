f5a20d5542def6c28998af1255815b1ccaa24b48
Daniil Shcherbakov <Charel@bk.ru>
2024-06-25 23:25:50 +0300

    Create MainViewModel

--- app/src/main/java/com/example/shoppinglist/domain/GetShopListUseCase.kt
+++ app/src/main/java/com/example/shoppinglist/domain/GetShopListUseCase.kt
@@ -1,6 +1,6 @@
 package com.example.shoppinglist.domain
 
-class GetShoppingListUseCase(private val shopListRepository: ShopListRepository) {
+class GetShopListUseCase(private val shopListRepository: ShopListRepository) {
 
     fun getShopList(): List<ShopItem>{
         return shopListRepository.getShopList()
