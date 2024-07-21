c6845e5cba3e16242cda8ef8ecf9b0494ae7fb97
Daniil Shcherbakov <Charel@bk.ru>
2024-07-21 12:29:32 +0300

    Work with ViewType RecyclerView

--- app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
@@ -10,23 +10,48 @@ import com.example.shoppinglist.domain.ShopItem
 
 class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
 
-    val list = listOf<ShopItem>()
+    companion object{
+        const val VIEW_TYPE_DISABLED = 2
+        const val VIEW_TYPE_ENABLED = 1
+
+        const val MAX_POOL_SIZE = 15
+    }
+
+    var shopList = listOf<ShopItem>()
+        set(value){
+            field =value
+            notifyDataSetChanged()
+        }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shop_enabled, parent, false)
+        val layoutItem = when(viewType){
+            VIEW_TYPE_ENABLED -> R.layout.item_shop_enabled
+            VIEW_TYPE_DISABLED -> R.layout.item_shop_disabled
+            else -> throw RuntimeException("Unknown view type $viewType")
+        }
+        val view = LayoutInflater.from(parent.context).inflate(layoutItem, parent, false)
         return ShopItemViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        return list.size
+        return shopList.size
     }
 
     override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
-        val shopItem = list[position]
+        val shopItem = shopList[position]
         holder.tvName.text = shopItem.name
         holder.tvCount.text = shopItem.count.toString()
         holder.view.setOnLongClickListener{
             true
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val shopItem = shopList[position]
+        if (shopItem.enabled){
+            return VIEW_TYPE_ENABLED
+        } else {
+            return VIEW_TYPE_DISABLED
         }
     }
 
