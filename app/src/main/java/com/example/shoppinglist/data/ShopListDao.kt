19f7e0b544b02568ff13aa090dcac3f0461751bf
Daniil Shcherbakov <Charel@bk.ru>
2024-11-16 13:20:48 +0300

    Delete data. Content provider

--- app/src/main/java/com/example/shoppinglist/data/ShopListDao.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListDao.kt
@@ -14,17 +14,23 @@ interface ShopListDao {
     @Query("SELECT * FROM shop_items")
     fun getShopList():LiveData<List<ShopItemDbModel>>
 
+    // Используем для обучения работы с  Content Provider
     @Query("SELECT * FROM shop_items")
     fun getShopListCursor():Cursor?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addShopItem(shopItemDbModel: ShopItemDbModel)
 
+    // Используем для обучения работы с  Content Provider
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun addShopItemSync(shopItemDbModel: ShopItemDbModel)
 
     @Query("DELETE FROM shop_items WHERE id=:shopItemId")
     suspend fun deleteShopItem(shopItemId: Int)
+
+    // Используем для обучения работы с  Content Provider
+    @Query("DELETE FROM shop_items WHERE id=:shopItemId")
+    fun deleteShopItemSync(shopItemId: Int): Int
 
     @Query("SELECT * FROM shop_items WHERE id=:shopItemId LIMIT 1")
     suspend fun getShopItem(shopItemId: Int): ShopItemDbModel
